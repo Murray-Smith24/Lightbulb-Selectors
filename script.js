8c612e545d6cc952843fae297ebc9d5a5c35eeaf
Murray-Smith24 <Murray.Smith@southwark.gov.uk>
2024-05-03 13:48:07 +0100

    added code for problem

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// Write your code here
+const bulb1 = document.querySelector("#lightbulb1");
+const bulb2 = document.querySelector("#lightbulb2");
+const bulb3 = document.querySelector("#lightbulb3");
+let output = document.querySelector("h3, .subtitle");
+let count = 0;
+
+function tally(event) {
+    count++;
+    output.innerHTML = "You've clicked the lights " + count +  " times";
+    event.target.classList.toggle("active");
+}
+
+bulb1.addEventListener("click", tally);
+bulb2.addEventListener("click", tally);
+bulb3.addEventListener("click", tally);
